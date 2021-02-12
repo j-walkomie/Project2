7f9fa99c6ce88e594d7b83b10971c79bd2fbe18a
j-walkomie <71896960+j-walkomie@users.noreply.github.com>
2021-02-12 15:45:28 -0500

    First commit of files

---
+++ fileio.h
@@ -0,0 +1,19 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+#include "../includes/constants.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+//attempt to open file 'filename' and read in all data
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int loadData(const std::string filename, vector<process> &myProcesses);
+
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int saveData(const std::string filename, vector<process> &myProcesses);
+
+
+#endif 
